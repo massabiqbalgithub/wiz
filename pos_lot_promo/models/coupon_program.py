014e4e168fa28a37ce9a18553f039c2ae335d062
massabiqbalgithub <115137701+massabiqbalgithub@users.noreply.github.com>
2022-12-15 14:25:11 +0500

    Add files via upload

--- pos_lot_promo/models/coupon_program.py
+++ pos_lot_promo/models/coupon_program.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 
 class CouponProgram(models.Model):
@@ -8,3 +8,9 @@ class CouponProgram(models.Model):
 
     lot_specific = fields.Boolean('Lot Specific',defult=False)
     specific_lot_ids = fields.Many2many('stock.production.lot', string='Specific Lots')
+
+    @api.onchange('specific_lot_ids')
+    def _onchange_specific_lot_ids(self):
+        if self.lot_specific and self.specific_lot_ids:
+            lot_product_domain = [('id','in',self.specific_lot_ids.mapped('product_id').ids)]
+            return {'domain': {'discount_specific_product_ids': lot_product_domain}}
--- pos_lot_promo/static/src/js/order.js
+++ pos_lot_promo/static/src/js/order.js
@@ -11,28 +11,30 @@ odoo.define('pos_lot_promo.pos', function (require) {
                 (program) => program.id == program_id);
             const order = this.pos.get_order();
             let selectedLine = order.get_selected_orderline()
-            if (promoProgram.lot_specific && selectedLine.has_product_lot){
-                var order_line_lots = selectedLine.get_lot_lines().map((line) =>(line.attributes['lot_name']))
-                order_line_lots.forEach(function(lot_name){
-                    var lot_domain = [['name', '=', lot_name]]
-                    rpc.query({
-                        model: 'stock.production.lot',
-                        method: 'search_read',
-                        args: [lot_domain, ['id']],
-                        kwargs: {
-                            limit: 1,
-                        }
-                    }).then(function (lot_id) {
-                        if (promoProgram.specific_lot_ids.includes(lot_id[0].id)){
-                            _order_super.applyPromotion.call(self, program_id);
-                        }
+            if (promoProgram.lot_specific){
+                if (selectedLine.has_product_lot){
+                    var order_line_lots = selectedLine.get_lot_lines().map((line) =>(line.attributes['lot_name']))
+                    order_line_lots.forEach(function(lot_name){
+                        var lot_domain = [['name', '=', lot_name]]
+                        rpc.query({
+                            model: 'stock.production.lot',
+                            method: 'search_read',
+                            args: [lot_domain, ['id']],
+                            kwargs: {
+                                limit: 1,
+                            }
+                        }).then(function (lot_id) {
+                            if (promoProgram.specific_lot_ids.includes(lot_id[0].id)){
+                                _order_super.applyPromotion.call(self, program_id);
+                            }
+                        })
                     })
-                })
+
+                }
             }
-                // if ( promoProgram && !(order.activePromoProgramIds.includes(program_id))) {
-                //     this.activePromoProgramIds.push(promoProgram.id);
-                //     this.trigger('update-rewards');
-                // }
+            else{
+                _order_super.applyPromotion.call(self, program_id);
+            }
         },
     })
 })
