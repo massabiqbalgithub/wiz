88c4a3447f763edbd81204f5509391e53973fd4e
massabiqbalgithub <115137701+massabiqbalgithub@users.noreply.github.com>
2022-12-19 11:48:13 +0500

    Add files via upload

--- pos_lot_promo/static/src/js/order.js
+++ pos_lot_promo/static/src/js/order.js
@@ -1,40 +1,45 @@
 odoo.define('pos_lot_promo.pos', function (require) {
     'use strict';
     const models = require('point_of_sale.models');
-    var rpc = require('web.rpc');
 
-    var _order_super = models.Order.prototype;
     models.Order = models.Order.extend({
-        applyPromotion: async function (program_id) {
-            var self = this;
-            const promoProgram = this.pos.promo_programs.find(
-                (program) => program.id == program_id);
-            const order = this.pos.get_order();
-            let selectedLine = order.get_selected_orderline()
-            if (promoProgram.lot_specific){
-                if (selectedLine.has_product_lot){
-                    var order_line_lots = selectedLine.get_lot_lines().map((line) =>(line.attributes['lot_name']))
-                    order_line_lots.forEach(function(lot_name){
-                        var lot_domain = [['name', '=', lot_name]]
-                        rpc.query({
-                            model: 'stock.production.lot',
-                            method: 'search_read',
-                            args: [lot_domain, ['id']],
-                            kwargs: {
-                                limit: 1,
+        _getSpecificDiscount: function (program, coupon_id, productRewards) {
+            const productIdsToAccount = new Set();
+            const amountsToDiscount = {};
+            
+            var self = this
+            for (let line of this._getRegularOrderlines()) {
+                // Lot Specific Logic
+                if (program.lot_specific){
+                    if (line.has_product_lot){
+                        var lot_name = line.get_lot_lines()[0].attributes['lot_name']
+                        var lot_id = this.pos.production_lots.find(lot => lot.name == lot_name)
+                        if (program.specific_lot_ids.includes(lot_id.id)){
+                            const key = self._getGroupKey(line);
+                            if (!(key in amountsToDiscount)) {
+                                amountsToDiscount[key] = line.get_quantity() * line.price;
+                            } else {
+                                amountsToDiscount[key] += line.get_quantity() * line.price;
                             }
-                        }).then(function (lot_id) {
-                            if (promoProgram.specific_lot_ids.includes(lot_id[0].id)){
-                                _order_super.applyPromotion.call(self, program_id);
-                            }
-                        })
-                    })
-
+                            productIdsToAccount.add(line.get_product().id);
+                        } 
+                    }    
+                } 
+                if (!(program.lot_specific) && program.discount_specific_product_ids.has(line.get_product().id)) {
+                    // Odoo promotion code (not lot specific)
+                    const key = this._getGroupKey(line);
+                    if (!(key in amountsToDiscount)) {
+                        amountsToDiscount[key] = line.get_quantity() * line.price;
+                    } 
+                    else {
+                        amountsToDiscount[key] += line.get_quantity() * line.price;
+                    }
+                    productIdsToAccount.add(line.get_product().id);
                 }
             }
-            else{
-                _order_super.applyPromotion.call(self, program_id);
-            }
+            this._considerProductRewards(amountsToDiscount, productIdsToAccount, productRewards);
+            return this._createDiscountRewards(program, coupon_id, amountsToDiscount);
         },
     })
+
 })
